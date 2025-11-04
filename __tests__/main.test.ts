874ca3f84e419e178c702fafdd306d18b2d9c630
Arthur Fleischman <cabralfle@gmail.com>
2025-11-04 17:00:24 +0100

    fixing ci.yml file

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -23,43 +23,19 @@ const openReleaseMock = jest.spyOn(keygenUtils, 'openRelease');
 const timeRegex = /^\d{2}:\d{2}:\d{2}/
 
 // Mock the GitHub Actions core library
-let debugMock: jest.SpyInstance
-let errorMock: jest.SpyInstance
+// let debugMock: jest.SpyInstance
+// let errorMock: jest.SpyInstance
 let getInputMock: jest.SpyInstance
 let setFailedMock: jest.SpyInstance
 let setOutputMock: jest.SpyInstance
-let release_id = "";
 
 describe('action', () => {
-  // beforeAll(async () => {
-  //   const response = await fetch(`https://api.keygen.sh/v1/accounts/${process.env.KEYGEN_ACCOUNT_ID}/environments`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/vnd.api+json",
-  //       "Accept": "application/vnd.api+json",
-  //       "Authorization": `Bearer ${process.env.KEYGEN_ADMIN_TOKEN}`
-  //     },
-  //     body: JSON.stringify({
-  //       "data": {
-  //         "type": "environments",
-  //         "attributes": {
-  //           "name": "Sandbox Environment",
-  //           "code": "sandbox"
-  //         }
-  //       }
-  //     })
-  //   })
 
-  //   const { data, errors } = await response.json()
-  //   console.log(data);
-  //   environment_id = data.data.id;
-  //   console.log(errors)
-  // })
   beforeEach(() => {
     jest.clearAllMocks()
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
-    errorMock = jest.spyOn(core, 'error').mockImplementation()
+    // debugMock = jest.spyOn(core, 'debug').mockImplementation()
+    // errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
     setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
@@ -74,13 +50,13 @@ describe('action', () => {
           return env.KEYGEN_ACCOUNT_ID!;
         case 'keygen-package-id':
           return env.KEYGEN_PACKAGE_ID!;
-        case 'keygen-product-id':
+        case 'keygen-productID':
           return env.KEYGEN_PRODUCT_ID!;
         case 'release-name':
           return 'test-release';
         case 'release-channel':
           return 'dev';
-        case 'github-tag':
+        case 'githubTag':
           return 'v1.0.0';
         default:
           return '';
@@ -91,59 +67,4 @@ describe('action', () => {
     expect(setOutputMock).toHaveBeenCalled()
 
   })
-
-  //   it('sets the time output', async () => {
-  //     // Set the action's inputs as return values from core.getInput()
-  //     getInputMock.mockImplementation((name: string): string => {
-  //       switch (name) {
-  //         case 'milliseconds':
-  //           return '500'
-  //         default:
-  //           return ''
-  //       }
-  //     })
-
-  //     await main.run()
-  //     expect(runMock).toHaveReturned()
-
-  //     // Verify that all of the core library functions were called correctly
-  //     expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-  //     expect(debugMock).toHaveBeenNthCalledWith(
-  //       2,
-  //       expect.stringMatching(timeRegex)
-  //     )
-  //     expect(debugMock).toHaveBeenNthCalledWith(
-  //       3,
-  //       expect.stringMatching(timeRegex)
-  //     )
-  //     expect(setOutputMock).toHaveBeenNthCalledWith(
-  //       1,
-  //       'time',
-  //       expect.stringMatching(timeRegex)
-  //     )
-  //     expect(errorMock).not.toHaveBeenCalled()
-  //   })
-
-  //   it('sets a failed status', async () => {
-  //     // Set the action's inputs as return values from core.getInput()
-  //     getInputMock.mockImplementation((name: string): string => {
-  //       switch (name) {
-  //         case 'milliseconds':
-  //           return 'this is not a number'
-  //         default:
-  //           return ''
-  //       }
-  //     })
-
-  //     await main.run()
-  //     expect(runMock).toHaveReturned()
-
-  //     // Verify that all of the core library functions were called correctly
-  //     expect(setFailedMock).toHaveBeenNthCalledWith(
-  //       1,
-  //       'milliseconds not a number'
-  //     )
-  //     expect(errorMock).not.toHaveBeenCalled()
-  //   })
-  // })
 })
